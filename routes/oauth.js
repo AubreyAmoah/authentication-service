ebd6a8f49a8167be876fd83b393f478f6fdfef09
AubreyAmoah <2gee419@gmail.com>
2025-08-27 10:44:34 +0000

    fixed ouath issues

--- routes/oauth.js
+++ routes/oauth.js
@@ -1,45 +1,24 @@
 const express = require('express');
-const roleController = require('../controllers/roleController');
-const { authenticate, requirePermission } = require('../middleware/auth');
-const { validateRequest, validateParams, roleSchemas, paramSchemas } = require('../utils/validation');
+const { oauthController } = require('../plugins/oauth');
+const { authenticate } = require('../middleware/auth');
 
 const router = express.Router();
 
-// All routes require authentication
+// Google OAuth routes
+router.get('/google', oauthController.googleLogin);
+router.get('/google/callback', oauthController.googleCallback);
+
+// GitHub OAuth routes
+router.get('/github', oauthController.githubLogin);
+router.get('/github/callback', oauthController.githubCallback);
+
+// Protected OAuth management routes
 router.use(authenticate);
 
-// Get available permissions
-router.get('/permissions', requirePermission('roles.read'), roleController.getAvailablePermissions);
+// Get linked providers
+router.get('/providers', oauthController.getLinkedProviders);
 
-// Get role statistics
-router.get('/stats', requirePermission('roles.read'), roleController.getRoleStats);
-
-// Check user permission
-router.get('/check-permission', requirePermission('roles.read'), roleController.checkUserPermission);
-
-// Get all roles in organization
-router.get('/', requirePermission('roles.read'), roleController.getRoles);
-
-// Create new role
-router.post('/', requirePermission('roles.create'), validateRequest(roleSchemas.create), roleController.createRole);
-
-// Get specific role
-router.get('/:id', requirePermission('roles.read'), validateParams(paramSchemas.uuid), roleController.getRoleById);
-
-// Update role
-router.patch('/:id', requirePermission('roles.update'), validateParams(paramSchemas.uuid), validateRequest(roleSchemas.update), roleController.updateRole);
-
-// Delete role
-router.delete('/:id', requirePermission('roles.delete'), validateParams(paramSchemas.uuid), roleController.deleteRole);
-
-// Duplicate role
-router.post('/:id/duplicate', requirePermission('roles.create'), validateParams(paramSchemas.uuid), roleController.duplicateRole);
-
-// Role user management
-router.get('/:id/users', requirePermission('roles.read'), validateParams(paramSchemas.uuid), roleController.getRoleUsers);
-
-// Role assignment (handled in user routes as well, but also available here)
-router.post('/assign', requirePermission('roles.assign'), validateRequest(roleSchemas.assignRole), roleController.assignRoleToUser);
-router.delete('/assign/:userId/:roleId', requirePermission('roles.assign'), roleController.removeRoleFromUser);
+// Unlink OAuth provider
+router.delete('/providers/:provider', oauthController.unlinkProvider);
 
 module.exports = router;
